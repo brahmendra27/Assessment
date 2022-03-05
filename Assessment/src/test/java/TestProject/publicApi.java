b255fd83f21361b7d5338b97715b73133926fedb
Brahmendra <brahmendrasdet@gmail.com>
2022-03-05 15:39:43 -0800

    added Assessment test

---
+++ Assessment/src/test/java/TestProject/publicApi.java
@@ -0,0 +1,27 @@
+package TestProject;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class publicApi {
+	
+	
+	@Test
+	public void GetCurrencycodesandnames() { 
+		// Specify the base URL to the RESTful web service 
+		RestAssured.baseURI = "https://api.coinbase.com/v2/currencies"; 
+		// Get the RequestSpecification of the request to be sent to the server. 
+		RequestSpecification httpRequest = RestAssured.given(); 
+		// specify the method type (GET) and the parameters if any. 
+		//In this case the request does not take any parameters 
+		Response response = httpRequest.request(Method.GET, "");
+		// Print the status and message body of the response received from the server 
+		System.out.println("Status received => " + response.getStatusLine()); 
+		System.out.println("Response=>" + response.prettyPrint());
+	}
+
+}
